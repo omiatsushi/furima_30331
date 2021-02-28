03ac8853cb0fb5b334688f6517adcbd4ca0e001d
omiatsushi <73770109+omiatsushi@users.noreply.github.com>
2021-02-28 16:19:58 +0900

    商品出品機能(修正1)

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -34,6 +34,18 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price is not a number")
       end
 
+      it '販売価格が299以下の場合登録できない' do
+        @item.price = '299'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+
+      it '販売価格が10000000以上の場合登録できない' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+
       it '画像が空の場合登録できない' do
         @item.image = nil
         @item.valid?
