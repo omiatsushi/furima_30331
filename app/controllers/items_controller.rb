b92fb4e0238251272327c14390d29e4299ba8482
omiatsushi <73770109+omiatsushi@users.noreply.github.com>
2021-03-06 18:40:18 +0900

    商品情報編集機能(修正1)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
 
+  before_action :set_item, only: [:show, :edit, :update, :move_to_index]
   before_action :move_to_index, only: [:update, :edit, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   
@@ -21,15 +22,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -45,10 +43,14 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
     unless current_user == @item.user
       redirect_to root_path
     end
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 
 end
